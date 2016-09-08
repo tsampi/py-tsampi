ba82139c91ee3c1943812ca936f884061178c246
tim watts <tim@readevalprint.com>
2016-09-08 08:31:35 +0000

    add github.com to known_hosts

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,8 @@
 set -e
 
 
+###### These should be in an init function
+
 # Commands to make kubernetes happy because you cannot set permission bits in the config and readonly means known_hosts cannot be updated
 chmod 400 ~/.ssh/id_rsa || echo "Cannot set permsissions on id_rsa for some reason"
 chmod 600 ~/.gnupg/gpg.conf &&  chmod 700 ~/.gnupg || echo "cannot set permissions on gnupg dir"
@@ -9,6 +11,10 @@ chmod 600 ~/.gnupg/gpg.conf &&  chmod 700 ~/.gnupg || echo "cannot set permissio
 # load gpg private keys if available
 gpg --import ~/gpg_keys/*.key || echo "No gpg keys imported"
 
+echo 'github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==
+' >>  ~/.ssh/known_hosts
+
+####
 
 # Define help message
 show_help() {
